f3797ed37be4068c4c20b2e8fdba7ef5b52b1ae6
Emma Yuan <emmayyw@gmail.com>
2021-04-24 12:34:01 -0700

    empty boilerplate; obj-C bridging header included

---
+++ sp21-cat/sp21-cat-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//
+//  Header.h
+//  sp21-cat
+//
+//  Created by Emma Yuan on 4/24/21.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ sp21-cat/sp21-cat.m
@@ -0,0 +1,8 @@
+//
+//  sp21-cat.m
+//  sp21-cat
+//
+//  Created by Emma Yuan on 4/24/21.
+//
+
+#import <Foundation/Foundation.h>
