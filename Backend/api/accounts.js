199240154b01ce384411b681befe76b87409e525
Siddharth Nag <59549474+siddharthnag@users.noreply.github.com>
2021-05-18 12:31:48 -0700

    Update user signup

--- Backend/api/accounts.js
+++ Backend/api/accounts.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
 const User = require('../models/user');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    const user = await User.find().exec();
+    res.status(200).json({ user });
+});
+
 router.post('/signup', async (req, res) => {
     const { user } = req.body;
-    const { email, name, password } = user;
-    if (!email || !name || !password) {
+    if (!user.email || !user.name || !user.password) {
         res.status(400).json({ error: 'Invalid input' });
     } else {
-        User.find({ 'email': email }, async function(err, existingUser) {
-            if (existingUser != null) {
-                res.status(400).json({ error: 'Email already exists' });
-            } else {
-                const newUser = await User.create(user);
-                res.status(200).json({ newUser });
-            }
-        });
+        const existingUser = await User.findOne({ email: user.email }); 
+        if (existingUser) {
+            res.status(400).json({ error: 'Email already exists', existingUser });
+        } else {
+            const newUser = await User.create(user);
+            const salt = await bcrypt.genSalt(10);
+            newUser.password = await bcrypt.hash(user.password, salt);
+            newUser.save();
+            res.status(200).json({ newUser });
+        }
     }
 });
 
